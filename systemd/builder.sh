a3fb11299e319ea01109976d65afec2259dbef9a
7Ji <pugokushin@gmail.com>
2023-02-17 19:46:19 +0800

    systemd/builder.sh: more robust

--- systemd/builder.sh
+++ systemd/builder.sh
@@ -1,7 +1,9 @@
 # This should only be run on the build machine, NOT the dev machine, it will break the local changes!
 git fetch --all
 git reset --hard origin/master
-releases=($(ls releases/ArchLinuxARM-*))
+git submodule init
+git submodule update
+releases=($(ls releases/ArchLinuxARM-*)) || true
 if [[ "${#releases[@]}" -gt 8 ]]; then # Only keep 8
   rm -f ${releases[@]::((${#releases[@]}-8))}
 fi
