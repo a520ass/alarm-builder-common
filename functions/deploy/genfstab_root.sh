ad7ac752cee3d369ae6b6c1cb03283fa9ed3c224
7Ji <pugokushin@gmail.com>
2023-02-24 20:56:37 +0800

    functions/deploy/genfstab_root: simplify to one-liner

--- functions/deploy/genfstab_root.sh
+++ functions/deploy/genfstab_root.sh
@@ -1,12 +1,5 @@
 genfstab_root() {
   echo " => Generating fstab..."
-  local fstab_file="${dir_root}/etc/fstab"
-  local fstab_content=$(
-    printf '# root partition with ext4 on SDcard / USB drive\nUUID=%s\t/\text4\trw,noatime,data=writeback\t0 1\n# boot partition with vfat on SDcard / USB drive\nUUID=%s\t/boot\tvfat\trw,noatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,utf8,errors=remount-ro\t0 2\n' "${uuid_root}" "${uuid_boot_specifier}"
-  )
-  local fstab_cache=$(mktemp)
-  echo "${fstab_content}" > "${fstab_cache}"
-  sudo cp "${fstab_cache}" "${fstab_file}"
-  rm -f "${fstab_cache}"
+  printf '# root partition with ext4 on SDcard / USB drive\nUUID=%s\t/\text4\trw,noatime,data=writeback\t0 1\n# boot partition with vfat on SDcard / USB drive\nUUID=%s\t/boot\tvfat\trw,noatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,utf8,errors=remount-ro\t0 2\n' "${uuid_root}" "${uuid_boot_specifier}" | sudo install -DTm 644 '/dev/stdin' "${dir_root}/etc/fstab"
   echo " => Generated fstab"
 }
