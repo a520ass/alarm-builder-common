37ce1caadd30e8f246e9e53032b898e7210b436d
7Ji <pugokushin@gmail.com>
2023-05-04 23:48:07 +0800

    bootstrap: non-interactive, ensure mountpoint

--- functions/cross/bootstrap/bootstrap_aarch64.sh
+++ functions/cross/bootstrap/bootstrap_aarch64.sh
@@ -8,7 +8,9 @@ bootstrap_aarch64() {
     local script_in_path='/root/bootstrap.sh'
     local script_actual_path="${cross_root}${script_in_path}"
     sudo install -Dm755 "${script_out_path}" "${script_actual_path}"
+    sudo mount -o bind "${cross_root}" "${cross_root}"
     sudo arch-chroot "${cross_root}" "${script_in_path}"
+    sudo umount "${cross_root}"
     sudo rm -f "${script_actual_path}"
     sudo touch "${bootstrapped_mark}"
   fi
