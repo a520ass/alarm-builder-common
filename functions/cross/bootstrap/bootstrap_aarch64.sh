591110ee5cda4514832058e843273b426d5236c2
7Ji <pugokushin@gmail.com>
2023-05-05 02:01:17 +0800

    change home owner on alarm

--- functions/cross/bootstrap/bootstrap_aarch64.sh
+++ functions/cross/bootstrap/bootstrap_aarch64.sh
@@ -8,6 +8,7 @@ bootstrap_aarch64() {
     echo '#!/bin/bash' > "${script_out_path}"
     cat 'common/scripts/cross_bootstrap.sh' >> "${script_out_path}"
     echo "usermod -u ${UID} alarm" >> "${script_out_path}"
+    echo "chown -R ${UID}:${UID} /home/alarm" >> "${script_out_path}"
     local script_in_path='/root/bootstrap.sh'
     local script_actual_path="${cross_root}${script_in_path}"
     sudo install -Dm755 "${script_out_path}" "${script_actual_path}"
