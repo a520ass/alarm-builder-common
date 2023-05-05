5dca6bdfad9b1c13741f82480635788d1c13fa50
7Ji <pugokushin@gmail.com>
2023-05-05 13:54:48 +0800

    cross: bootstrap: install deps

--- scripts/cross_bootstrap.sh
+++ scripts/cross_bootstrap.sh
@@ -1,5 +1,15 @@
 pacman -Rcns linux-aarch64 vi --noconfirm
 pacman-key --init
 pacman-key --populate
-pacman -Syu base-devel git sudo --noconfirm
+pacman -Syu --noconfirm \
+  arch-install-scripts \
+  base-devel \
+  dosfstools \
+  git \
+  go \
+  parted \
+  uboot-tools \
+  xz \
+  wget \
+  sudo
 echo 'alarm ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/alarm_allow_sudo_no_passwd
