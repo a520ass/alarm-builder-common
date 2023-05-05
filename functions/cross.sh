693f54c2e927b440fbcdf64341621b74c7e80a1b
7Ji <pugokushin@gmail.com>
2023-05-05 13:52:34 +0800

    cross: use a naive loop to copy stuffs

--- functions/cross.sh
+++ functions/cross.sh
@@ -7,10 +7,18 @@ cross() {
   local in_project='/home/alarm/alarm-builder'
   local cross_project="${cross_root}${in_project}"
   sudo mkdir -p "${cross_project}"
-  {
-    shopt -s extglob
-    sudo cp -rva !(${dir_cross}) "${cross_project}/"
-  }
+  # !(${dir_cross}) does not work even with shopt -s extglob, have to do it this way
+  local copy_list=()
+  local copy_target=''
+  for copy_target in *; do
+    case "${copy_target}" in 
+      "${dir_cross}") : ;;
+      *)
+        copy_list+=("${copy_target}")
+      ;;
+    esac
+  done
+  sudo cp -rva "${copy_list[@]}" "${cross_project}/"
   sudo mount -o bind "${cross_root}" "${cross_root}"
   sudo arch-chroot "${cross_root}" "${in_project}/common/scripts/cross_entrypoint.sh"
   sudo umount -R "${cross_root}"
