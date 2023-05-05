bd233c654d5ba1830222b515ad8608cf73b0bb5c
7Ji <pugokushin@gmail.com>
2023-05-05 23:19:39 +0800

    start_distccd: fix typo

--- functions/cross/start_distccd_and_trap.sh
+++ functions/cross/start_distccd_and_trap.sh
@@ -3,6 +3,6 @@ start_distccd_and_trap() {
   PATH="$(readlink -f ${dir_cross}/xtools_aarch64_on_x86_64)/aarch64-unknown-linux-gnu/bin:${PATH}" \
     distccd --daemon --allow-private --pid-file "${pid_file}"
   local distccd_pid="$(<"${pid_file}")"
-  echo " => Staretd distccd daemon at pid ${distccd_pid}"
+  echo " => Started distccd daemon at pid ${distccd_pid}"
   trap "kill -s INT ${distccd_pid}" INT TERM EXIT
 }
