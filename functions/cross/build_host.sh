0a5c31e866c3add3ea434f1d23ce1d08bbbb85d7
7Ji <pugokushin@gmail.com>
2023-05-06 17:19:33 +0800

    build_host: finish

--- functions/cross/build_host.sh
+++ functions/cross/build_host.sh
@@ -1,20 +1,27 @@
+relative_source ../prepare/prepare_pkg/should_build.sh
+
 build_host() {
   if [[ -d "${dir_build_cross}" ]]; then
     echo " => Building host part for cross build packages"
-    pushd "${dir_build_cross}"
+    pushd "${dir_build_cross}" > /dev/null
     local build_pkg
     for build_pkg in *; do
       if [[ -d "${build_pkg}" ]]; then
-        if [[ -d  ${dir_build}/${build_pkg} ]]; then
-          pushd "${build_pkg}"
-          ./build.sh
-          popd
+        dir_build_pkg="${dir_build}/${build_pkg}"
+        if [[ -d "${dir_build_pkg}" ]]; then
+          pushd "${dir_build_pkg}" > /dev/null
+          if should_build "${build_pkg}"; then
+            pushd "${build_pkg}" > /dev/null
+            ./build.sh
+            popd > /dev/null
+          fi
+          popd > /dev/null
         else
           echo "  -> Ignored cross package ${build_pkg} since we did no find its generic counterpart under ${dir_build}"
-          return 1
         fi
       fi
     done
-    popd
+    popd > /dev/null
   fi
+  echo " => Built all host parts for cross build packages"
 }
