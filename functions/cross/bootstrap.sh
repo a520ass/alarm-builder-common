35209944e569995ec8b6bf1f450ace70c384b23b
7Ji <pugokushin@gmail.com>
2023-05-06 14:12:05 +0800

    cross: bootstrap: fix import

--- functions/cross/bootstrap.sh
+++ functions/cross/bootstrap.sh
@@ -1,5 +1,6 @@
 relative_source bootstrap/bootstrap_aarch64.sh
 relative_source bootstrap/bootstrap_xtools_aarch64_on_x86_64.sh
+relative_source bootstrap/bootstrap_x86_64.sh
 
 bootstrap() {
   echo " => Bootstrapping cross build environment..."
