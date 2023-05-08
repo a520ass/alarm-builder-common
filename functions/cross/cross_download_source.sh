0aaa1524d1725bde5e465d1de5de374c8bbf5a4b
7Ji <pugokushin@gmail.com>
2023-05-08 14:29:28 +0800

    cross_download_source: just use makepkg lib from aarch64

--- functions/cross/cross_download_source.sh
+++ functions/cross/cross_download_source.sh
@@ -3,7 +3,7 @@ relative_source ../prepare/prepare_blob.sh
 
 cross_download_source() {
   echo " => Pre-downloading sources for cross build"
-  local arch_root="$(readlink -f ${dir_cross}/x86_64)"
+  local arch_root="$(readlink -f ${dir_cross}/aarch64)"
   LIBRARY="${arch_root}/usr/share/makepkg" \
   MAKEPKG_CONF="${arch_root}/etc/makepkg.conf" \
     download_source
