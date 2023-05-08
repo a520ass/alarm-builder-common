7d1b030aa76759d2ad52f840b052a584cb2fc224
7Ji <pugokushin@gmail.com>
2023-05-08 14:28:15 +0800

    bootstrap: no need to bootstrap x64 arch

--- functions/cross/bootstrap.sh
+++ functions/cross/bootstrap.sh
@@ -1,6 +1,6 @@
 relative_source bootstrap/bootstrap_aarch64.sh
 relative_source bootstrap/bootstrap_xtools_aarch64_on_x86_64.sh
-relative_source bootstrap/bootstrap_x86_64.sh
+# relative_source bootstrap/bootstrap_x86_64.sh
 
 bootstrap_mt() {
   echo " => Bootstrapping cross build environment..."
@@ -8,7 +8,7 @@ bootstrap_mt() {
   local pid_bootstrap_aarch64=$!
   bootstrap_xtools_aarch64_on_x86_64 &
   local pid_bootstrap_xtools_aarch64_on_x86_64=$!
-  bootstrap_x86_64 &
+  # bootstrap_x86_64 &
   local pid_bootstrap_x86_64=$!
   echo " => Waiting for all cross build environment bootstrapping to finish"
   wait $pid_bootstrap_aarch64 $pid_bootstrap_x86_64 $pid_bootstrap_xtools_aarch64_on_x86_64
@@ -18,6 +18,6 @@ bootstrap() {
   echo " => Bootstrapping cross build environment..."
   bootstrap_aarch64
   bootstrap_xtools_aarch64_on_x86_64
-  bootstrap_x86_64
+  # bootstrap_x86_64
   echo " => Bootstrapped cross build environment"
 }
