bc636548e720555590ffd164811d099ec5974cdf
7Ji <pugokushin@gmail.com>
2023-05-08 14:55:23 +0800

    download_source: use subshell to isolate to avoid libmakepkg breaking our functions

--- functions/prepare/prepare_pkg/download_source.sh
+++ functions/prepare/prepare_pkg/download_source.sh
@@ -4,25 +4,27 @@ download_source() {
   local LIBRARY="${LIBRARY:-'/usr/share/makepkg'}"
   local SRCDEST=.
   local lib=
-  for lib in "$LIBRARY"/*.sh; do
-    source "$lib"
-  done
-  load_makepkg_config
-  pushd "${dir_build}" > /dev/null
-  local build_pkg=
-  for build_pkg in *; do
-    if [[ -d "${build_pkg}" ]]; then
-      pushd "${build_pkg}" > /dev/null
-      if should_build "${build_pkg}"; then
-        echo "  -> Downloading source for ${build_pkg} before build.."
-        (
-          source_safe PKGBUILD
-          download_sources novcs allarch
-        )
+  (
+    for lib in "$LIBRARY"/*.sh; do
+      source "$lib"
+    done
+    load_makepkg_config
+    pushd "${dir_build}" > /dev/null
+    local build_pkg=
+    for build_pkg in *; do
+      if [[ -d "${build_pkg}" ]]; then
+        pushd "${build_pkg}" > /dev/null
+        if should_build "${build_pkg}"; then
+          echo "  -> Downloading source for ${build_pkg} before build.."
+          (
+            source_safe PKGBUILD
+            download_sources novcs allarch
+          )
+        fi
+        popd > /dev/null
       fi
-      popd > /dev/null
-    fi
-  done
-  popd > /dev/null
+    done
+    popd > /dev/null
+  )
   echo " => Downloaded all sources before build"
 }
